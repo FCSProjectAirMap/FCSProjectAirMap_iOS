5f860e2183cb2eea4a32ba7dc07afadf95130331
Mijeong Jeon <ninevincentg@gmail.com>
2016-07-05 18:08:31 +0900

    test

---
+++ AirMap/addedFile.h
@@ -0,0 +1,13 @@
+//
+//  addedFile.h
+//  
+//
+//  Created by Mijeong Jeon on 7/5/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface addedFile : NSObject
+
+@end
---
+++ AirMap/addedFile.m
@@ -0,0 +1,13 @@
+//
+//  addedFile.m
+//  
+//
+//  Created by Mijeong Jeon on 7/5/16.
+//
+//
+
+#import "addedFile.h"
+
+@implementation addedFile
+
+@end
