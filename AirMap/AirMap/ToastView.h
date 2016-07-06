6108cd0b5e86eaaeeda774bcee93b27cb7bf0818
Mijeong Jeon <ninevincentg@gmail.com>
2016-07-06 20:52:18 +0900

    2016-07-06 Add ToastView
    * Add ToastView when authorization for photo is denied
    * Make custom view and label
    * Use animation to show toast-like view
    * Add tap gesture recognizer to move setting area

---
+++ AirMap/AirMap/ToastView.h
@@ -0,0 +1,15 @@
+//
+//  ToastView.h
+//  AirMap
+//
+//  Created by Mijeong Jeon on 7/6/16.
+//  Copyright © 2016 FCSProjectAirMap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ToastView : UIView
+
++ (void)showToastInView:(UIView *)view;
+
+@end
